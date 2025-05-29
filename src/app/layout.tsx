5c017c8d0f0a8e120dc1e49b804c6ca1e14984f2
Ney Robson Araujo Gama <neyrobson.dev@gmail.com>
2025-05-29 08:26:10 -0300

    feat: first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { baselightTheme } from "@/utils/theme/DefaultColors";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider theme={baselightTheme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
